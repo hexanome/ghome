0a3f441798e96b96260ba913ffce22988c6c5ed1
alefoulon <alexandre.lefoulon@insa-lyon.fr>
2012-01-10 10:40:54 +0100

    -addding listener .h and .c.

---
+++ modulesTraduction/EnOceanModuleTraduction/EnOceanModuleTraduction/Listener.c
@@ -0,0 +1,9 @@
+//
+//  Listener.c
+//  EnOceanModuleTraduction
+//
+//  Created by Alexandre Lefoulon on 10/01/12.
+//  Copyright (c) 2012 INSA Lyon. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ modulesTraduction/EnOceanModuleTraduction/EnOceanModuleTraduction/Listener.h
@@ -0,0 +1,14 @@
+//
+//  Listener.h
+//  EnOceanModuleTraduction
+//
+//  Created by Alexandre Lefoulon on 10/01/12.
+//  Copyright (c) 2012 INSA Lyon. All rights reserved.
+//
+
+#ifndef EnOceanModuleTraduction_Listener_h
+#define EnOceanModuleTraduction_Listener_h
+
+
+
+#endif
