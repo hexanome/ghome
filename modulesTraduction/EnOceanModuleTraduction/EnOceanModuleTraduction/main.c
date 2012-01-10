ea6ec09cc8c55199f0daa40b4b5e96a32d3af720
alefoulon <alexandre.lefoulon@insa-lyon.fr>
2012-01-10 10:38:22 +0100

    -adding project, be careful to commit only .c and . h files.

---
+++ modulesTraduction/EnOceanModuleTraduction/EnOceanModuleTraduction/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  EnOceanModuleTraduction
+//
+//  Created by Alexandre Lefoulon on 10/01/12.
+//  Copyright (c) 2012 INSA Lyon. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
