6191e0338e8524cad6432c77367daa1da1cc1855
Alexandre Lefoulon <alexandre.lefoulon@insa-lyon.fr>
2012-02-14 08:35:11 +0100

    creation de la trame actionneur, préparation à l'envoi.

--- modulesTraduction/EnOceanModuleActuator/EnOceanModuleActuator/Sender.c
+++ modulesTraduction/EnOceanModuleActuator/EnOceanModuleActuator/Sender.c
@@ -18,8 +18,23 @@ char stringToBinary (char str[]) {
 
 tcpFrameType tcpFrameCreationWith(idValue value){
     tcpFrameType frame;
-    frame.H_SEQLENGTH = stringToBinary("3B");
-    
-    
-    
+    frame.H_SEQLENGTH = stringToBinary("6B");
+    frame.ID_BYTE0 = stringToBinary("03");
+    frame.ID_BYTE1 = stringToBinary("FF");
+    frame.ID_BYTE2 = stringToBinary("9F");
+    frame.ID_BYTE3 = stringToBinary("1E");
+    // A remplacer par la value.
+    if (value.value == '1') {
+        frame.DATA_BYTE0 = stringToBinary("50");
+        frame.DATA_BYTE1 = stringToBinary("00");
+        frame.DATA_BYTE2 = stringToBinary("00");
+        frame.DATA_BYTE3 = stringToBinary("00");
+               
+    }else{
+        frame.DATA_BYTE0 = stringToBinary("51");
+        frame.DATA_BYTE1 = stringToBinary("00");
+        frame.DATA_BYTE2 = stringToBinary("00");
+        frame.DATA_BYTE3 = stringToBinary("00");
+    }
+    frame.CHECKSUM = stringToBinary("01");    
 }
