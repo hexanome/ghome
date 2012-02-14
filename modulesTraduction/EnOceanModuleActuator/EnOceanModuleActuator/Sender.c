8ff0392e7f4510541eb886632bffd4280d0b24bd
Alexandre Lefoulon <alexandre.lefoulon@insa-lyon.fr>
2012-02-14 09:20:44 +0100

    push test xavier

--- modulesTraduction/EnOceanModuleActuator/EnOceanModuleActuator/Sender.c
+++ modulesTraduction/EnOceanModuleActuator/EnOceanModuleActuator/Sender.c
@@ -16,6 +16,34 @@ char stringToBinary (char str[]) {
     return CTOX(str[1]) + 16 * CTOX(str[0]);
 }
 
+SOCKET socketConnexion(void)
+{
+    
+    int erreur = 0;
+    
+    SOCKET sock;
+    SOCKADDR_IN sin;
+    
+    if(!erreur)
+    {
+        /* Création de la socket */
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        
+        /* Configuration de la connexion */
+        sin.sin_addr.s_addr = inet_addr(ADRESSE);
+        sin.sin_family = AF_INET;
+        sin.sin_port = htons(PORT);
+        
+        /* Si le client arrive à se connecter */
+        if(connect(sock, (SOCKADDR*)&sin, sizeof(sin)) != SOCKET_ERROR)
+            printf("Connexion à %s sur le port %d\n", inet_ntoa(sin.sin_addr), htons(sin.sin_port));
+        else
+            printf("Impossible de se connecter\n");
+    }
+    
+    return sock;
+}
+
 tcpFrameType tcpFrameCreationWith(idValue value){
     tcpFrameType frame;
     frame.H_SEQLENGTH = stringToBinary("6B");
@@ -37,4 +65,10 @@ tcpFrameType tcpFrameCreationWith(idValue value){
         frame.DATA_BYTE3 = stringToBinary("00");
     }
     frame.CHECKSUM = stringToBinary("01");    
+    frame.STATUS = stringToBinary("01");
+    return frame;
+}
+
+void convertToBuffer(tcpFrameType frame, char* buffer){
+    
 }
