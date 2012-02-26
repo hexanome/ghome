810aa22cfd27eb0d38dcb12a1b339f58127d5113
Matthieu Coquet <matthieu.coquet@insa-lyon.fr>
2012-02-26 14:01:46 -0800

    ajout classe Benchmark

--- gmem/gmem.c
+++ gmem/gmem.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "gmem.h"
 
-#define HEAP_SIZE 60000
+#define HEAP_SIZE 16000000
 #define WORD_SIZE sizeof(unsigned int)
 #define MIN_BLOCK sizeof(struct block)
 
@@ -13,7 +13,6 @@ struct block * p; // empty
 
 /* Initialize GMem */
 void init() {
-  printf("init\n");
   p = (struct block *) mem;
   p->size = HEAP_SIZE;
   p->next = p;
@@ -32,55 +31,41 @@ void * gmalloc(unsigned int size) {
   }
   void * ptr = p;
   unsigned int s = max(MIN_BLOCK, size + (size % WORD_SIZE) + WORD_SIZE);
-  printf("gmalloc: %u wanted, taking %u\n", size, s);
   while (p->next->size != s &&  p->next->size < s + sizeof(struct block)) {
-    printf("p->next->size = %u, s = %u\n", p->next->size, s);
     if (p->next + p->next->size == p->next->next) {
-      printf("merge\n");
       p->next->size += p->next->next->size;
       p->next->next = p->next->next->next;
     } else {
-      printf("next\n");
       p = p->next;
       if (p == ptr) {
-        printf("not enough memory\n");
         return (void *) -1;
       }
     }
   }
-  printf("gmalloc: %u free\n", p->next->size);
   ptr = p->next;
   if (p->next->size == s) {
     if (p->next == p->next->next) {
       p = 0;
     } else {
       p->next = p->next->next;
-      printf("setting p to %p, p->next is %p\n", p, p->next);
     }
   } else {
     p->next = ptr + s;
     p = p->next;
-    printf("p->size = %u, p->next = %p\n", p->size, p->next);
     p->next = (struct block *) ((struct block *) ptr)->next;
-    printf("p->size = %u, p->next = %p\n", p->size, p->next);
     p->size = (unsigned int) ((struct block *) ptr)->size - s;
-    printf("p->size = %u, p->next = %p\n", p->size, p->next);
   }
   *((unsigned int *) ptr) = s;
-  printf("allocated pointer %p to block of size %u\n", ptr, s);
   return ptr + WORD_SIZE;
 }
 
 /* Free a previously allocated block */
 void gfree(void * ptr) {
-  printf("gfree called on %p\n", ptr);
   if (ptr == (void *)-1) {
-    printf("nothing to free\n");
     return;
   }
   struct block * b = ptr - WORD_SIZE;
   unsigned int s = *((unsigned int *) b);
-  printf("gfree: %u\n", s);
   b->size = s;
   if (p == 0) {
     p = b;
@@ -88,7 +73,6 @@ void gfree(void * ptr) {
     while ( ! (p->next <= p && b < p->next) && ! (b < p->next && b > p ) && ! (p->next <= p && b > p) ) {
       p = p->next;
     }
-    printf("inserting block %p between %p and %p\n", b, p, p->next);
     b->next = p->next;
   }
   p->next = b;
