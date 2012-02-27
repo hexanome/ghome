001ffdb56ec5225fde55d3e93f258676f2262036
Jan Keromnes <janx@linux.com>
2012-02-27 12:56:20 +0100

    [UI] Display real sensor data
    Signed-off-by: Jan Keromnes <janx@linux.com>

--- serv/routes/index.js
+++ serv/routes/index.js
@@ -1,24 +1,53 @@
 
-// Import other modules
+// Import useful modules
 var actuators = require('../../rules/comp/actuators.js'),
     com = require('../../rules/comp/com.js'),
     sensors = require('../../rules/comp/sensors.js');
 
-var sensors = {};
 
-sensors.getSensorPropertyValues (function (err,res) {
-  if(err) throw err;
-  for (var i = 0 ; i < res.length ; i++) {
-    sensors[res.sensorId] = sensors[res.sensorId] || {};
-    sensors[res.sensorId][res.sensorPropertyId] = res.value;
+// Variables we work on
+var s = [],     // s = [ { name: 'Sensor1', val: '3°C' }, ... ]
+    sname = {}, // sname[sensorId] = sensorName
+    pname = {}; // pname[propertyId] = propertyName
+
+
+// Get sensor IDs and names
+sensors.getSensors (function (err, res) {
+  if (err) throw err;
+
+  for (var i = 0; i < res.length; i++) {
+    sname[res[i].id] = res[i].name;
   }
-  console.log(sensors);
+
+  // Get property IDs and names
+  sensors.getSensorProperties(function (err, res) {
+    if (err) throw err;
+
+    for (var i = 0; i < res.length; i++) {
+      pname[res[i].id] = ''; // TODO '°C'? 'km'? 'L'?
+    }
+
+    // Get values measured by the sensors
+    sensors.getSensorPropertyValues (function (err,res) {
+      if (err) throw err;
+
+      for (var i = 0 ; i < res.length ; i++) {
+        var sensor = {
+          name: sname[res[i].sensorId],
+          val: res[i].value + pname[res[i].sensorPropertyId]
+        };
+        if (sensor.name) s.push(sensor);
+      }
+
+    });
+  });
 });
 
+
+// Render Express™ pages
 exports.index = function(req, res) {
   res.render('index', {
     // mothereffing template key:values
-    sensors : [{id:'s1',val:'42°C'}, {id:'s2',val:'closed'}]
+    sensors : s
   });
 };
-
