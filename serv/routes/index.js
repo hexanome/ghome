7e4c50244ffcb303df9a1927399ac66cc877876b
Jan Keromnes <janx@linux.com>
2012-02-27 11:11:42 +0100

    [UI] Integration in progress
    Signed-off-by: Jan Keromnes <janx@linux.com>

--- serv/routes/index.js
+++ serv/routes/index.js
@@ -1,11 +1,17 @@
 
-/*
- * GET home page.
- */
+// Import other modules
+var actuators = require('../../rules/comp/actuators.js'),
+    com = require('../../rules/comp/com.js'),
+    sensors = require('../../rules/comp/sensors.js');
 
-exports.index = function(req, res){
+sensors.getSensors(function(thing){
+  console.error(JSON.stringify(thing));
+});
+
+exports.index = function(req, res) {
   res.render('index', {
     // mothereffing template key:values
+    sensors : [{id:'s1',val:'42°C'}, {id:'s2',val:'closed'}]
   });
 };
 
