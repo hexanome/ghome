e22892b9933974652f5b7dbb454017eee6dcdaab
Jan Keromnes <janx@linux.com>
2012-02-28 04:45:51 +0100

    [UI] Add new Sensor Type works
    - Yay!
    
    Signed-off-by: Jan Keromnes <janx@linux.com>

--- server/web/js/superScript.js
+++ server/web/js/superScript.js
@@ -1,4 +1,10 @@
-$(document).ready(function() {
-  // Déclaration des popup modaux.
-  $("#newTypeModal").modal({ show : false });
+Scout('#newSensorType').on('click', function (query) {
+  query.action = 'addSensorType';
+  query.data = {
+    name: Scout('#newSensorTypeName').value
+  };
+  query.resp = function (typeId) {
+    console.log('New Sensor Type sais yo!',typeId);
+    document.location = '/sensor-types';
+  }
 });
